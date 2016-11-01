c2fc3a9fd4a823cb4c6d211aca358b191ac9a716
Libby Coy <libbycoy23@gmail.com>
2016-11-01 15:28:11 +1100

    added black squares

--- app/assets/javascripts/Preloader.js
+++ app/assets/javascripts/Preloader.js
@@ -9,7 +9,7 @@ Heist.Preloader = function (game) {
 Heist.Preloader.prototype = {
 
   preload: function () {
-        this.load.image('background', 'assets/phaser_background-02.png');
+        this.load.image('background', 'assets/cement-05.png');
         this.load.image('opacity', 'assets/opacity-02.png');
         this.load.image('levelOneBackground', 'assets/menu-back-02.png');
         this.load.image('levelTwoBackground', 'assets/menu-back-03.png');
